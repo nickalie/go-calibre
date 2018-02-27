457d1b42c2aa4fee73415c3ce5a62d3f5ea36f78
Nikolay Glushchenko <nick@nickalie.com>
2018-02-27 22:25:57 +0200

    fixed tests

--- calibre_test.go
+++ calibre_test.go
@@ -7,39 +7,90 @@ import (
 	"net/http"
 	"io"
 	"github.com/stretchr/testify/assert"
+	"archive/zip"
+	"path/filepath"
 )
 
 func init() {
-	downloadFile("https://www.gutenberg.org/ebooks/2600.epub.images?session_id=6326d908280f40b489a0b3be7a2653349aa8774d", "source.epub")
-	downloadFile("https://royallib.com/get/fb2/tolstoy_lev/voyna_i_mir_kniga_1.zip", "source.zip")
+	downloadFile("https://royallib.com/get/epub/tolstoy_lev/voyna_i_mir_tom_1.zip", "source1.zip", true)
+	downloadFile("https://royallib.com/get/fb2/tolstoy_lev/voyna_i_mir_kniga_1.zip", "source2.zip", false)
 }
 
-func downloadFile(url, target string) {
+func downloadFile(url, target string, extract bool) {
 	_, err := os.Stat(target)
 
+	if err == nil {
+		if extract {
+			extractEpub(target)
+		}
+		return
+	}
+
+	resp, err := http.Get(url)
+
 	if err != nil {
-		resp, err := http.Get(url)
+		fmt.Printf("Error while downloading test file: %v\n", err)
+		panic(err)
+	}
 
-		if err != nil || resp.StatusCode != http.StatusOK {
-			fmt.Printf("Error while downloading test image: %v\n", err)
-			panic(err)
+	defer resp.Body.Close()
+
+	f, err := os.Create(target)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if extract {
+		extractEpub(target)
+	}
+}
+
+func extractEpub(value string) {
+	zipReader, err := zip.OpenReader(value)
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, v := range zipReader.File {
+		if filepath.Ext(v.Name) != ".epub" {
+			continue
 		}
 
-		defer resp.Body.Close()
+		zipToFile(v, "source.epub")
+	}
+}
 
-		f, err := os.Create(target)
+func zipToFile(zipFile *zip.File, target string) {
+	f, err := os.Create(target)
 
-		if err != nil {
-			panic(err)
-		}
+	if err != nil {
+		panic(err)
+	}
 
-		defer f.Close()
+	defer f.Close()
 
-		_, err = io.Copy(f, resp.Body)
+	zipReader, err := zipFile.Open()
 
-		if err != nil {
-			panic(err)
-		}
+	if err != nil {
+		panic(err)
+	}
+
+	defer zipReader.Close()
+
+	_, err = io.Copy(f, zipReader)
+
+	if err != nil {
+		panic(err)
 	}
 }
 
@@ -49,11 +100,11 @@ func TestConvertEpub(t *testing.T) {
 }
 
 func TestConvertFB2(t *testing.T) {
-	err := Convert("source.zip", "target.mobi")
+	err := Convert("source2.zip", "target.mobi")
 	assert.Nil(t, err)
 }
 
 func TestConvertError(t *testing.T) {
-	err := Convert("source1.zip", "target2.mobi")
+	err := Convert("source3.zip", "target2.mobi")
 	assert.NotNil(t, err)
 }
